ee780fa1afadb1f99b427a3f12eb77f9db89c99a
LorenzoChukwuebuka <Lawrenceobi2@gmail.com>
2022-08-15 06:18:39 +0100

    done

--- src/views/Admin/pendingdeptdues.vue
+++ src/views/Admin/pendingdeptdues.vue
@@ -3,6 +3,7 @@
     <adminnav msg="Admin Dashboard" />
 
     <div class="container-fluid mt-2">
+      
       <h4 class="text-dark text-center">Pending Departmental Dues</h4>
 
       <div class="row justify-content-center mt-5">
