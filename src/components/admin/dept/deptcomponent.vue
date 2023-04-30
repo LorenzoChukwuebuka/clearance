732425826e7da5d2f9c226493a002a159815ac66
LorenzoChukwuebuka <Lawrenceobi2@gmail.com>
2023-04-30 18:48:54 +0100

    done

--- src/components/admin/dept/deptcomponent.vue
+++ src/components/admin/dept/deptcomponent.vue
@@ -1,29 +1,24 @@
 <template>
-  <main>
-    <div class="container-fluid mt-2">
-      <h4 class="text-dark text-center">Add Department</h4>
-      <addDept :schools="schools" @form="getForm" />
+	<main>
+		<div class="container-fluid mt-2">
+			<h4 class="text-dark text-center">Add Department</h4>
+			<addDept :schools="schools" @form="getForm" />
 
-      <div class="row justify-content-center mt-5">
-        <div class="container col-8">
-          <div class="bg-dark" v-if="success.length">
-            <span v-for="(suc, index) in success" :key="index">
-              <b-alert show variant="danger" dismissible>
-                {{ suc }}
-              </b-alert>
-            </span>
-          </div>
+			<div class="row justify-content-center mt-5">
+				<div class="container col-8">
+					<div class="bg-dark" v-if="success.length">
+						<span v-for="(suc, index) in success" :key="index">
+							<b-alert show variant="danger" dismissible>
+								{{ suc }}
+							</b-alert>
+						</span>
+					</div>
 
-          <fetchdeptComponent
-            :depts="depts"
-            @Id="delDept"
-            :schools="schools"
-            @edit="updateDept"
-          />
-        </div>
-      </div>
-    </div>
-  </main>
+					<fetchdeptComponent :depts="depts" @Id="delDept" :schools="schools" @edit="updateDept" />
+				</div>
+			</div>
+		</div>
+	</main>
 </template>
 
 <script>
@@ -31,65 +26,66 @@ import addDept from "@/components/admin/dept/addDeptComponent.vue";
 import fetchdeptComponent from "@/components/admin/dept/fetchDept.vue";
 
 export default {
-  name: "deptcomponent",
-  components: {
-    addDept,
-    fetchdeptComponent,
-  },
-  data() {
-    return {
-      schools: [],
-      depts: [],
-      success: [],
-    };
-  },
-  mounted() {
-    this.getSchool();
-    this.getDept();
-  },
-  methods: {
-    getSchool() {
-      this.$http.get("${this.$PORT}school").then((res) => {
-        this.schools = res.data;
-      });
-    },
-    getForm(form) {
-      this.$http
-        .post(`${this.$PORT}Dept`, form)
-        .then((res) => {
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    getDept() {
-      this.$http
-        .get(`${this.$PORT}Dept`)
-        .then((res) => {
-          this.depts = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    delDept(Id) {
-      this.$http
-        .delete(`${this.$PORT}Dept/${Id}`)
-        .then((res) => {
-          this.success.push(res.data.message);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    updateDept(edit) {
-      this.$http.put(`${this.$PORT}Dept/${edit.id}`, edit).then((res) => {
-        console.log(res.data);
-      });
-    },
-  },
+	name: "deptcomponent",
+	components: {
+		addDept,
+		fetchdeptComponent,
+	},
+	data() {
+		return {
+			schools: [],
+			depts: [],
+			success: [],
+		};
+	},
+	mounted() {
+		this.getSchool();
+		this.getDept();
+	},
+	methods: {
+		getSchool() {
+			this.$http.get("${this.$PORT}school").then((res) => {
+				this.schools = res.data;
+			});
+		},
+		getForm(form) {
+			this.$http
+				.post(`${this.$PORT}Dept`, form)
+				.then((res) => {
+					console.log(res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		getDept() {
+			this.$http
+				.get(`${this.$PORT}Dept`)
+				.then((res) => {
+					this.depts = res.data;
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		delDept(Id) {
+			this.$http
+				.delete(`${this.$PORT}Dept/${Id}`)
+				.then((res) => {
+					this.success.push(res.data.message);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		updateDept(edit) {
+			this.$http.put(`${this.$PORT}Dept/${edit.id}`, edit).then((res) => {
+				console.log(res.data);
+			});
+		},
+	},
 };
 </script>
 
-<style></style>
+<style>
+</style>
--- src/views/Admin/createAdmin.vue
+++ src/views/Admin/createAdmin.vue
@@ -1,29 +1,29 @@
 <template>
-  <main>
-    <adminnav msg="Admin Dashboard" />
+	<main>
+		<adminnav msg="Admin Dashboard" />
 
-    <main>
-      <div class="container-fluid mt-2">
-        <h4 class="text-dark text-center">Add Admin</h4>
+		<main>
+			<div class="container-fluid mt-2">
+				<h4 class="text-dark text-center">Add Admin</h4>
 
-        <addAdmin @form="submitForm" />
+				<addAdmin @form="submitForm" />
 
-        <div class="row justify-content-center mt-5">
-          <div class="container col-8">
-            <div class="bg-dark" v-if="success.length">
-              <span v-for="(suc, index) in success" :key="index">
-                <b-alert show variant="danger" dismissible>
-                  {{ suc }}
-                </b-alert>
-              </span>
-            </div>
+				<div class="row justify-content-center mt-5">
+					<div class="container col-8">
+						<div class="bg-dark" v-if="success.length">
+							<span v-for="(suc, index) in success" :key="index">
+								<b-alert show variant="danger" dismissible>
+									{{ suc }}
+								</b-alert>
+							</span>
+						</div>
 
-            <fetchAdmin :admins="admins" @Id="delAdmin" @edit="updateAdmin" />
-          </div>
-        </div>
-      </div>
-    </main>
-  </main>
+						<fetchAdmin :admins="admins" @Id="delAdmin" @edit="updateAdmin" />
+					</div>
+				</div>
+			</div>
+		</main>
+	</main>
 </template>
 
 <script>
@@ -32,69 +32,70 @@ import addAdmin from "@/components/admin/adminComp/addAdmin.vue";
 import fetchAdmin from "@/components/admin/adminComp/fetchAdmin.vue";
 
 export default {
-  name: "createAdmin",
-  components: {
-    adminnav,
-    addAdmin,
-    fetchAdmin,
-  },
-  data() {
-    return {
-      success: [],
-      admins: [],
-    };
-  },
-  mounted() {
-    this.getAdmins();
-  },
-  methods: {
-    submitForm(form) {
-      this.$http
-        .post(`${this.$PORT}admin`, form)
-        .then((res) => {
-          if (res.data.message == "inserted successfully") {
-            this.success.push(res.data.message);
-          } else {
-            alert("User already Exists");
-          }
-        })
-        .catch((err) => console.log(err));
-    },
-    getAdmins() {
-      this.$http
-        .get(`${this.$PORT}admin`)
-        .then((res) => {
-          this.admins = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
+	name: "createAdmin",
+	components: {
+		adminnav,
+		addAdmin,
+		fetchAdmin,
+	},
+	data() {
+		return {
+			success: [],
+			admins: [],
+		};
+	},
+	mounted() {
+		this.getAdmins();
+	},
+	methods: {
+		submitForm(form) {
+			this.$http
+				.post(`${this.$PORT}admin`, form)
+				.then((res) => {
+					if (res.data.message == "inserted successfully") {
+						this.success.push(res.data.message);
+					} else {
+						alert("User already Exists");
+					}
+				})
+				.catch((err) => console.log(err));
+		},
+		getAdmins() {
+			this.$http
+				.get(`${this.$PORT}admin`)
+				.then((res) => {
+					this.admins = res.data;
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
 
-    delAdmin(Id) {
-      if (confirm("Are you sure ?")) {
-        this.$http.delete(`${this.$PORT}admin/${Id}`);
-        setTimeout(() => {
-          window.location.reload();
-        }, 2000);
-      }
-    },
-    updateAdmin(edit) {
-      if (confirm("Are you sure you want to update this ?")) {
-        this.$http
-          .put(`${this.$PORT}admin/${edit.id}`, edit)
-          .then((res) => {
-            if (res.data.message === "updated") {
-              alert(res.data.message);
-              setTimeout(() => {
-                window.location.reload();
-              }, 2000);
-            }
-          });
-      }
-    },
-  },
+		delAdmin(Id) {
+			if (confirm("Are you sure ?")) {
+				this.$http.delete(`${this.$PORT}admin/${Id}`);
+				setTimeout(() => {
+					window.location.reload();
+				}, 2000);
+			}
+		},
+		updateAdmin(edit) {
+			if (confirm("Are you sure you want to update this ?")) {
+				this.$http
+					.put(`${this.$PORT}admin/${edit.id}`, edit)
+					.then((res) => {
+						if (res.data.message === "updated") {
+							alert(res.data.message);
+							setTimeout(() => {
+								window.location.reload();
+							}, 2000);
+						}
+					});
+			}
+		},
+	},
 };
 </script>
 
-<style></style>
+<style>
+</style>
